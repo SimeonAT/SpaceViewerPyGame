4efe403fee8283aac81983d211cbcdcba719ea4d
Simeon Tran <ilikedragonslol@gmail.com>
2021-03-25 23:59:47 -0700

    Formatted and removed unecessary comments in spritesheet.py

--- Version 0.2/spritesheet.py
+++ Version 0.2/spritesheet.py
@@ -1,33 +1,42 @@
-""" Contains a function that converts a spritesheet into a list of
-    individual sprite frames.
-    - https://stackoverflow.com/questions/6239769/how-can-i-crop-an-image-with-pygame
-    - https://stackoverflow.com/questions/36653519/how-do-i-get-the-size-width-x-height-of-my-pygame-window """
-import pygame
+"""
+Contains a function that converts a spritesheet into a list of
+individual sprite frames.
+- https://stackoverflow.com/questions/6239769/how-can-i-crop-an-image-with-pygame
+- https://stackoverflow.com/questions/36653519
+  /how-do-i-get-the-size-width-x-height-of-my-pygame-window
+"""
 
-def get_frames(rows, frames_per_row, frame_width, frame_height, hanging_frames = None):
-    """@params: rows -> how many rows in the spritesheet
-                frames_per_row -> how many frames in each row
-                frame_width, frame_height -> the dimensions of each frame in the spritesheet
-                hanging_frames -> contains how many frames are in last row when last row < frames_per_row;
-                                  'None' means that last row has frames_per_row frames  """
-    frames = []  # the list of frames from sprite to return
-    num_frames = 0  # how many frames does the spritesheet have?
 
-    # The coordinates of the top left corner of each frame; starts with top left corner of first frame
+def get_frames(rows, frames_per_row, frame_width, frame_height, hanging_frames=None):
+    """
+    Returns a list containing the position of each individual sprite frame in the spritesheet,
+    and the number of frames that are in the spritesheet.
+
+    rows -> how many rows in the spritesheet
+    frames_per_row -> how many frames in each row
+    frame_width, frame_height -> the dimensions of each frame in the spritesheet
+    hanging_frames -> contains how many frames are in last row when last row < frames_per_row;
+                      'None' means that last row has frames_per_row frames
+    """
+    frames = []
+    num_frames = 0
+
+    # The coordinates of top left corner of each frame; starts with top left corner of first frame
     x = 0
     y = 0
 
     # Use a for loop to create a new surface for each individual sprite
     # and store each sprite in the sprites list
     for i in range(1, rows + 1):
-        for j in range(1 , frames_per_row + 1):
+        for j in range(1, frames_per_row + 1):
             frames.append([x, y, frame_width, frame_height])
             num_frames += 1
 
-            x += frame_width  # Get ready for next sprite
+            # Get ready for next sprite
+            x += frame_width
 
             # If on last row and there are hanging frames, break once we got all the hanging frames
-            if hanging_frames != None and i == rows:
+            if hanging_frames is not None and i == rows:
                 if j >= hanging_frames:
                     break
 
