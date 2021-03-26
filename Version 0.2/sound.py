7fe3220ecfea5db44c52b1f9d39e8dea386c31d9
Simeon Tran <ilikedragonslol@gmail.com>
2021-03-25 23:52:57 -0700

    Formatted sound.py

--- Version 0.2/sound.py
+++ Version 0.2/sound.py
@@ -5,17 +5,20 @@ from random import randint
 from setup import resource_path
 
 
-""" A function that goes to each music folder, grabs all the file locations for each song. """
 def fill_music_lists(file_location, music_list):
-    """ @params: file_location -> location of folder with all the music songs
-                 music_list -> list to put file locations in """
+    """
+    Goes into each music folder, and grabs all of the file locations for each song.
+    - file_location -> location of folder with all of the songs
+    - music_list -> list to put file locations in
+    """
     for filename in os.listdir(file_location):
         if filename.endswith(".wav"):
-            music_list.append(file_location + "/" +  filename)
+            music_list.append(file_location + "/" + filename)
 
     # Make each file location in music_list compatible with PyInstaller
     for song in music_list:
         song = resource_path(song)
+
 
 # Organize game music by function (main loop music, battle music, etc):
 main_loop = []
@@ -23,15 +26,18 @@ fill_music_lists(os.path.join("Music"), main_loop)
 
 
 def initialize_music():
-    """ Initialize the music library. """
+    """ Initializes the music library. """
     pygame.mixer.init()
 
-def play_music(music_list):
-    """ Cycles through each song and loops back to playing each one
-        after playing all of them.
-        Got some help here: https://stackoverflow.com/questions/7746263/how-can-i-play-an-mp3-with-pygame """
 
-    if not pygame.mixer.music.get_busy():  # if a song is not playing, play a random main loop song
+def play_music(music_list):
+    """
+    Cycles through each song and loops back to playing each one after playing all of them.
+    Got help here: https://stackoverflow.com/questions/7746263/how-can-i-play-an-mp3-with-pygame
+    """
+
+    # if a song is not playing, play a random song
+    if not pygame.mixer.music.get_busy():
         random_num = randint(0, len(music_list) - 1)
         pygame.mixer.music.load(resource_path(music_list[random_num]))
         pygame.mixer.music.play()
