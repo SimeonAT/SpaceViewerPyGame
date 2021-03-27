e24f9504ec54038fe131f7fb6943d790753eaa3c
Simeon Tran <ilikedragonslol@gmail.com>
2021-03-26 23:30:10 -0700

    Formatted code for draw() function.

--- Version 0.2/text_box.py
+++ Version 0.2/text_box.py
@@ -53,21 +53,22 @@ class TextBox(pygame.sprite.Sprite):
                          (CENTER_Y - self.final_size[1] / 2) + 250)
         return
 
-
-    def draw(self, screen, frames_since_shown, key_pressed = None):
+    def draw(self, screen, frames_since_shown, key_pressed=None):
         """
         Displays the textbox onto the screen.
 
         screen: the screen to draw text box on
         frames_since_shown: how many frames has it appeared on screen so far
-        key_pressed: doesn't do anything in this function, but needed as some text boxes use key_pressed 
+        key_pressed: holds the key player pressed in current frame
         """
-        
+
         """ --- THE TEXT BOX APPEARANCE TRANSITION --- """
         if frames_since_shown <= 30:
-            # Reset self.current_line and self.current_letter to give RPG dialogue transition effect
+            # If textbox just popped up:
+            # - reset current line and current letter to give RPG dialogue transition effect
+            # - exit function early to avoid displaying textbox in its initial frames
             self.current_line = self.current_letter = 1
-            return   # don't transition if the object just popped up
+            return
 
         # The actual transition:
         elif frames_since_shown == 31:
@@ -81,46 +82,56 @@ class TextBox(pygame.sprite.Sprite):
         elif frames_since_shown == 35:
             self.current_size = [self.final_size[0], self.final_size[1]]
 
-        # self.image is the original img to transform; self.current_image is the transformed img to be displayed on screen
-        self.current_image = pygame.transform.scale(self.image, (self.current_size[0], self.current_size[1]))
-        screen.blit(self.current_image, ( (CENTER_X - self.current_size[0] / 2),
-                                  (CENTER_Y -  self.current_size[1] / 2) + 250 ))  # display image at given coordinates
+        # self.image is the original img to transform;
+        # self.current_image is the transformed img to be displayed on screen
+        self.current_image = pygame.transform.scale(self.image,
+                                                    (self.current_size[0], self.current_size[1]))
 
-        # Display text on the textbox
+        # Display current image to the bottom center half of the screen
+        screen.blit(self.current_image, ((CENTER_X - self.current_size[0] / 2),
+                                         (CENTER_Y - self.current_size[1] / 2) + 250))
+
+        # Display text on the textbox; we render one new line each frame so that each line
+        # "pops up" like how they do in a typical RPG.
         if frames_since_shown > 35:
-            font = pygame.font.Font(resource_path(os.path.join("Graphics", "m5x7.ttf")), 40)  # Upload the m5x7 font by Daniel Linssen
+            font = pygame.font.Font(resource_path(os.path.join("Graphics", "m5x7.ttf")), 40)
 
-            """ Display what is intended to be displayed:
-                Render one new line each frame so that each line "pops up" like how they do in a typical RPG. """
+            # coordinates of the top left corner of the first line of text
             top_left = [CENTER_X - (self.final_size[0] / 2) + 100,
-                        CENTER_Y - (self.final_size[1] / 2) + 375]  # coordinates of the top left corner of the first line of text
+                        CENTER_Y - (self.final_size[1] / 2) + 375]
 
             for i in range(0, self.current_line):
                 if i >= len(self.lines):
-                    # Double check if we just rendered the last line,
-                    # if we do, leave loop and don't get ready for next line (because there isn't a next line to get ready for)
+                    # Double check if we just rendered the last line;
+                    # if we did, leave loop, because there isn't a "next line" to get ready for
                     break
 
                 if i == self.current_line - 1:
-                    line_to_render = self.lines[i][0:self.current_letter - 1]   # slice string up to where current_letter is "referring to"
+                    # slice string up to where current_letter is "referring to"
+                    line_to_render = self.lines[i][0:self.current_letter - 1]
                 else:
-                    line_to_render = self.lines[i]  # render whole line
+                    # render whole line
+                    line_to_render = self.lines[i]
 
                 text = font.render(line_to_render, True, (255, 255, 255, 255))
                 screen.blit(text, dest=top_left)
-                top_left[1] += 40  # the next line of text will be under the previous line of text
-                if frames_since_shown % 0.25 == 0:   # a new letter appears every 0.25 frames
-                    if self.current_letter >= len(self.lines[i]):  # need >= to account for when self.current_letter is > than amount of lines
-                        # If we already printed out all letters for given line, get ready for next line
+                top_left[1] += 40
+
+                # a new letter appears every 0.25 frames
+                if frames_since_shown % 0.25 == 0:
+                    # need >= to account for when self.current_letter is > than amount of lines
+                    if self.current_letter >= len(self.lines[i]):
+                        # If all letters have been printed for given line, get ready for next line
                         self.current_line += 1
-                        self.current_letter = 1   # reset self.current_letter so that we can print first letter of next line
+
+                        # reset self.current_letter so that we can print first letter of next line
+                        self.current_letter = 1
                     else:
                         # if not finished printing out whole line, get ready to print next letter in next frame
                         self.current_letter += 1
 
-
-            close_text = font.render("Press ENTER to continue.", True, (255, 255, 255, 255))  # Textbox Call-To-Action
-            screen.blit(close_text, dest = (CENTER_X + 250, CENTER_Y + 290))
+            close_text = font.render("Press ENTER to continue.", True, (255, 255, 255, 255))
+            screen.blit(close_text, dest=(CENTER_X + 250, CENTER_Y + 290))
 
 
 class Extension_TextBox(TextBox):
