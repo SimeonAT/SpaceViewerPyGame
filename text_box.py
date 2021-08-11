f58f1584dfaea5bc67caab34ca69f3cc1b55f655
Simeon Tran <ilikedragonslol@gmail.com>
2021-08-10 21:52:14 -0700

    Formatted code in text_box.py

--- text_box.py
+++ text_box.py
@@ -209,13 +209,23 @@ class Extension_TextBox(TextBox):
 
 class Choice_TextBox(TextBox):
     """
-    A textbox child class that has the functionality to allow the user to make YES/NO choices
-    using the WASD keys.
+    A special textbox that allows the player to make a YES/NO style decision. A different
+    predefined set of textboxes will appear depending on the choice the player makes.
     """
 
     def __init__(self, size=(452, 93), lines=[], choices=None):
-        """ choices: a list that contains all of the possible strings that the user will see when
-                     using WASD or Arrow Keys to select options.
+        """
+            Parameters:
+                size: The size of the textbox, represented as a list/tuple. Default size
+                      is 452 x 93.
+                lines: The prompt the choice textbox will display, represented as a list of
+                       string, with each string representing a line.
+                choices: A list containing the two possible choices that the user can make.
+                         Set to "None" by default, as the parent textbox class does not
+                         have this functionality.
+
+            Return:
+                Does not return anything; just creates a new Choice Textbox object.
         """
         super().__init__(size, lines)
 
@@ -227,52 +237,62 @@ class Choice_TextBox(TextBox):
         self.choice_to_blit = 0
 
     def draw(self, screen, frames_since_shown, key_pressed = None):
-        """@params -> screen: the screen to draw text box on
-                      frames_since_shown: how many frames has it appeared on screen so far
-                      key_pressed: the key that was pressed by user input; 'None' means no key was pressed  """
+        """
+            Parameters:
+                screen: the screen object to draw the textbox on
+                frames_since_shown: How many frames has the textbox appeared in the screen so far
+                key_pressed: the key that was pressed by the user; "None" means that no
+                             key has been pressed
+
+            Returns:
+                Has no return value. The function just draws the textbox onto the PyGame screen.
+        """
         if frames_since_shown <= 10:
-            # Reset self.current_line and self.current_letter to give RPG dialogue transition effect
+            # Reset current line and letter to display an RPG dialogue transition effect 
             self.current_line = self.current_letter = 1
             return
 
-        # self.image is the original img to transform; self.current_image is the transformed img to be displayed on screen
-        self.current_image = pygame.transform.scale(self.image, (self.final_size[0], self.final_size[1]))
+        # self.image: the original img to transform
+        # self.current_image: the transformed img that will actually be displayed
+        self.current_image = pygame.transform.scale(self.image,
+                                                    (self.final_size[0], self.final_size[1]))
         screen.blit(self.current_image, ((CENTER_X - self.final_size[0] / 2),
-                                         (CENTER_Y - self.final_size[1] / 2) + 250))  # display image at given coordinates
+                                         (CENTER_Y - self.final_size[1] / 2) + 250))
 
-        # Display text on the textbox
-        font = pygame.font.Font(resource_path(os.path.join("Graphics", "m5x7.ttf")), 40)  # Upload the m5x7 font by Daniel Linssen
+        font = pygame.font.Font(resource_path(os.path.join("Graphics", "m5x7.ttf")), 40)
 
-        """ Display what is intended to be displayed:
-            Render one new line each frame so that each line "pops up" like how they do in a typical RPG. """
+        # coordinates of the top left corner of the first line of text
         top_left = [CENTER_X - (self.final_size[0] / 2) + 100,
-                    CENTER_Y - (self.final_size[1] / 2) + 375]  # coordinates of the top left corner of the first line of text
+                    CENTER_Y - (self.final_size[1] / 2) + 375]
 
+        # Render one new line each frame so that each line 
+        # "pops up" like how they do in a typical RPG.
         for i in range(0, self.current_line):
             if i >= len(self.lines):
-                # Double check if we just rendered the last line,
-                # if we do, leave loop and don't get ready for next line (because there isn't a next line to get ready for)
+                # If we rendered the last line, end the loop. 
                 break
 
             if i == self.current_line - 1:
-                line_to_render = self.lines[i][0:self.current_letter - 1]  # slice string up to where current_letter is "referring to"
+                # Slice the string up to index "current_letter"
+                line_to_render = self.lines[i][0:self.current_letter - 1]
             else:
-                line_to_render = self.lines[i]  # render whole line
+                # Render the whole line
+                line_to_render = self.lines[i]
 
             text = font.render(line_to_render, True, (255, 255, 255, 255))
             screen.blit(text, dest=top_left)
-            top_left[1] += 40  # the next line of text will be under the previous line of text
-            if frames_since_shown % 0.25 == 0:  # a new letter appears every 0.25 frames
-                if self.current_letter >= len(
-                        self.lines[i]):  # need >= to account for when self.current_letter is > than amount of lines
-                    # If we already printed out all letters for given line, get ready for next line
+
+            # The next line of text will only differ from previous line by only the vertical coord
+            top_left[1] += 40
+            if frames_since_shown % 0.25 == 0:
+                if self.current_letter >= len(self.lines[i]):
+                    # Get ready for the next line
                     self.current_line += 1
-                    self.current_letter = 1  # reset self.current_letter so that we can print first letter of next line
+                    self.current_letter = 1
                 else:
-                    # if not finished printing out whole line, get ready to print next letter in next frame
+                    # Get ready to print the next letter
                     self.current_letter += 1
 
-        """ Determine which choice to render """
         if key_pressed == "a":
             # If "A" is pressed, move option to YES
             self.choice_to_blit -= 1
@@ -280,12 +300,14 @@ class Choice_TextBox(TextBox):
             # if "D" is pressed, move option to NO
             self.choice_to_blit += 1
 
-        self.choice_to_blit %= len(self.choices)  # We % it so that the choice_to_blit will always be an index in self.choices
-        choice = font.render(self.choices[self.choice_to_blit], True, (255, 255, 255, 255))   # display the choice onto the screen
+        # We mod the choice to blit variable so that it is never an index outside of self.choices
+        self.choice_to_blit %= len(self.choices)
+        choice = font.render(self.choices[self.choice_to_blit], True, (255, 255, 255, 255))
         screen.blit(choice, dest = top_left)
 
-        close_text = font.render("Press ENTER to continue.", True, (255, 255, 255, 255))  # Textbox Call-To-Action
+        close_text = font.render("Press ENTER to continue.", True, (255, 255, 255, 255))
         screen.blit(close_text, dest=(CENTER_X + 250, CENTER_Y + 290))
 
-        return self.choice_to_blit     # return info on what text box was render by giving text box index in choices list
-                                       # this will allow us to know what the user picked (i.e. if they picked "YES or "NO")
+        # return info on what text box was render by giving text box index in choices list
+        # this will allow us to know what the user picked (i.e. if they picked "YES or "NO")
+        return self.choice_to_blit
