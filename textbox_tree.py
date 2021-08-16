480bdef856c751edd765ce8f5490b3f02fa69c2b
Simeon Tran <ilikedragonslol@gmail.com>
2021-08-15 21:09:09 -0700

    Starting to work on TextBox Tree ADT

---
+++ textbox_tree.py
@@ -0,0 +1,5 @@
+"""
+Rather that having to deal with the complicated logic of implementing response textboxes
+following a choice from a Choice Textbox, the TextBox tree allow for easy collection and
+management of textboxes.
+"""
