399b1dbde35ecab292bdabe6d66e3804b889aebd
Simeon Tran <ilikedragonslol@gmail.com>
2021-08-16 16:32:05 -0700

    Dealt with cases when the textbox in the tree is not a choice textbox, but has to render another textbox.

--- textbox_tree.py
+++ textbox_tree.py
@@ -11,7 +11,7 @@ import text_box
 
 class Textbox_Tree_Node:
 
-    def __init__(self, textbox_object, yes_child = None, no_child = None):
+    def __init__(self, textbox_object, yes_child = None, no_child = None, next_child = None):
         """
         Creates a new Textbox Tree Node.
 
@@ -19,10 +19,13 @@ class Textbox_Tree_Node:
             - the actual textbox object that the node points to
             - the textbox response for the YES and NO responses,
               if textbox object is a choice textbox
+            - the next textbox object to render, if textbox node represents a non-choice
+              textbox
         """
         self.textbox_object = textbox_object
         self.yes_child = yes_child
         self.no_child = no_child
+        self.next_child = next_child
         self.frames_since_shown = 0
         return
 
@@ -85,4 +88,23 @@ class Textbox_Tree:
         else:
             self.current = self.current.no_child
 
+        return
+
+    def next_textbox(self):
+        """
+        Renders the next textbox if current textbox is not a Choice Textbox.
+        """
+        if self.head.is_choice_textbox():
+            raise TypeError
+
+        self.frames_since_shown = 0
+        self.current = self.current.next_child
+        return
+
+    def reset_tree(self):
+        """
+        Resets the current textbox to the head textbox, so the textbox
+        interactions play out the same the next time these set of textboxes are rendered.
+        """
+        self.current = self.head
         return
