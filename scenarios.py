403099ccc091c99f537a7158b558d040d6da811e
Simeon Tran <ilikedragonslol@gmail.com>
2021-08-23 23:22:00 -0700

    Source file to manage all of the scenarios used by the game objects:

---
+++ scenarios.py
@@ -0,0 +1,6 @@
+"""
+This source file contains functions that returns a textbox tree representing
+a scenario/story that the game objects (i.e. spaceships, planets, asteroid belts) present.
+In other words, all of the combat and exploration gameplay interactions are determined
+by these functions.
+"""
