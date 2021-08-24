96fa7bac0e6978ec4f74a49bcc6c0582ca5dffba
Simeon Tran <ilikedragonslol@gmail.com>
2021-08-23 23:49:45 -0700

    Right now, I'm just trying to display the heart sprite at the top left of the screen

--- player.py
+++ player.py
@@ -1,5 +1,26 @@
 """
-This Python file houses the code for the Player class.
+This Python file houses the code for the Player class
+and the tutorial NPC that guides the player on how to play the game.
 """
 
-raise NotImplementedError
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+GRID_SIZE = [10, 10]
+
+CENTER_X = int(SCREEN_WIDTH / 2)
+CENTER_Y = int(SCREEN_HEIGHT / 2)
+
+class Player:
+
+    def __init__(self):
+        self.lives = 3
+        self.heart_image = pygame.image.load(resource_path(
+            os.path.join("Graphics", "Player Objects", "heart.png"))).covert_alpha()
+
+    def draw():
+        # The heart sprite, along with the number of lives the player has,
+        # will always be shown at the top left corner of the screen
+        screen.blit(self.heart_image,(0, 0))
+        return
+
